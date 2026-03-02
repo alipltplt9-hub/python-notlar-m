bc684e29cb977acd07eeac3583f137d17587c2d1
alipltplt9-hub <alipltplt9@gmail.com>
2026-03-02 20:15:04 +0300

    Add files via upload

---
+++ type-conversion.py
@@ -0,0 +1,51 @@
+# x=input('1.sayı:')
+# y=input('2.sayı:')
+# print(type(x))
+# print(type(y))
+
+# toplam=int(x)+int(y)
+# print(toplam)
+
+
+x=5   #int
+y=2.5    #float
+name='Ali'  #string
+# isOnline=True   #bool
+# print(type(x))
+# print(type(y))
+# print(type(name))
+# print(type(isOnline))
+#type conversion
+#int to float
+# x=float(x)
+# print(x)
+# print(type(x))
+#float to int
+# y=int(y)
+# print(y)
+# print(type(y))
+# result=str(x)+str(y)
+
+# print(result)
+# print(type(result))
+# isonline=True
+# isonline=str(isonline)
+# print(isonline)
+# print(type(isonline))
+#bool to int
+isonline=False
+isonline=int(isonline)
+print(isonline)
+print(type(isonline))
+
+
+
+
+
+
+
+
+#ctrl+k+c #seçili satırları yorum satırı yapar
+#ctrl+k+u #seçili satırların yorum satırını kaldırır
+#ctrl+z #yapılan işlemi geri alır
+#ctrl+y #geri alınan işlemi tekrar yapar
