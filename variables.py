6742c66302005dbf5bd8763c80976974c7d4f3ee
alipltplt9-hub <alipltplt9@gmail.com>
2026-03-02 17:55:37 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,36 @@
+print(5000-(5000*0.17))
+print(5000-(5000*0.17)-1000)
+print(5000-(5000*0.17)-1000-1000)
+print(5000-(5000*0.17)-1000-1000-1000)
+maasAli=5000
+maasAhmet=4000
+vergi = 0.27
+print(maasAli-(maasAli*vergi))
+print(maasAhmet-(maasAhmet*vergi))
+#değişken tanımlama kuralları
+#rakamla başlayamaz
+number1=10
+print (number1)
+number1+=30
+print (number1)
+#büyük küçük harf duyarlılığı vardır
+age=20
+Age=30
+print(age)
+#türkçe karakter kullanmamalıyız
+yas=21
+_age=21
+x=1      #int
+y=2.3       #float
+name="ali"   #string
+isStudent=True #bool
+# x,y,name,isStudent=1,2.3,"ali",True
+a='10'
+b='20'
+print(a+b) # =>1020
+firstName="Ali"
+lastName="polat"
+print (firstName+" "+lastName) #Ali Polat
+
+
+
