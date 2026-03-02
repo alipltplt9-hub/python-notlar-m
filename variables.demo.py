7f787b42b92aff34ab35934b15a48cb0012988d9
alipltplt9-hub <alipltplt9@gmail.com>
2026-03-02 18:24:43 +0300

    Add files via upload

---
+++ variables.demo.py
@@ -0,0 +1,36 @@
+"""
+   1-Bir Müşternin aşağidaki bilgileri için değişken oluşturunuz
+
+   Müşteri adi
+   Müşteri soyadi
+   Müşteri adi+soyadi
+   Müşteri cinsiyeti
+   Müşteri tc kiMlik
+   Müşteri doğuM yili
+   Müşteri adres bilgisi
+   Müşteri yaşi
+   """
+musteriAdi='Ali'
+musteriSoyadi='polat'
+musteriAdSoyad=musteriAdi + ' ' +musteriSoyadi
+musteriCinsiyet=True #erkek
+musteriTcKimlik='12345678910'
+musteriDoğumYili=2004
+musteriAdres='istanbul'
+musteriYas=2026-musteriDoğumYili
+"""
+2-aşağidaki siparişlerin toplam tutarini hesaplayiniz
+sipariş1='110'
+sipariş2='1100.5'
+sipariş3='356.95'
+"""
+sipariş1='110'
+sipariş2='1100.5'
+sipariş3='356.95'
+toplamTutar=float(sipariş1)+float(sipariş2)+float(sipariş3)
+print(toplamTutar)
+total=sipariş1+sipariş2+sipariş3
+print(total)
+
+
+#ctrl+h sağ üstte bir panel açar üstteki kutuya bir şey yaz alttaki kutuyada bir şey yaz üstte ne yazdıysan metindekilerin hepsi alttaki ile değişir bunu daha çok büyük küçük veya sesli sessiz harfler için yap
