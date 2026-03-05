bfe1743e867648df9903d9b85fc317ed7d72d8ef
alipltplt9-hub <alipltplt9@gmail.com>
2026-03-06 02:58:35 +0300

    Add files via upload

---
+++ type-conversion-demo.py
@@ -0,0 +1,23 @@
+"""Daire alanini ve çevresini hesaplayan program
+Daire Alani:πr²
+Daire Çevresi:2πr
+yarı çapı verilen daireninin alanini ve çevresini hesaplayan program(r:3.14)"""
+pi=3.14
+r=float(input("yarı çap:"))
+alan=pi*(r**2)
+Cevre=2*pi*r
+print("Daire Alanı:",alan)
+print("Daire Çevresi:",Cevre)
+
+
+
+
+
+
+
+#alt+0178 kare işareti
+#math.pow(x,y) fonksiyonu üs alma işlemi yapar:x'in y. kuvvetini verir
+#math.pi pi sayısını verir:3.14
+#math.sqrt(x) fonksiyonu karekök alma işlemi yapar:x'in karekökünü verir
+#math.ceil(x) fonksiyonu x sayısından büyük veya eşit en küçük tam sayıyı verir
+#math.floor(x) fonksiyonu x sayısından küçük veya eşit en büyük tam sayıyı verir
