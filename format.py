82551dbfe524d7d1978beb819bc6b1c17261d709
alipltplt9-hub <alipltplt9@gmail.com>
2026-03-06 18:36:55 +0300

    Add files via upload

---
+++ format.py
@@ -0,0 +1,16 @@
+name= 'Ali'
+surname='polat'
+age=21
+#print('my name is {} {}'.format(name, surname))
+#print('my name is {n} {s}'.format(n=name, s=surname))
+#print("my name is {} {} and I am {} years old".format(name,surname, '21'))
+# result=200/700
+# print('the result is {r:10.4}'.format(r=result))
+print(f"My name is {name} {surname} and I am {age} years old.")
+
+
+
+
+
+
+
