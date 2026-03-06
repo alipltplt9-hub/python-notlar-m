bcbf5ed2c60a0c00654d83c468bc675635e5ee63
alipltplt9-hub <alipltplt9@gmail.com>
2026-03-06 03:28:25 +0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,16 @@
+name = 'ali'
+surname='polat'
+age=21
+greeting='My name is ' + name + ' ' + surname + ' and \nI am ' + str(age) + ' years old.'
+#print(greeting)
+# print(greeting[0])
+# print(greeting[3])
+# print(greeting[0:5]) #0 ile 5 arasındaki karakterleri verir. 5 dahil değil.
+# print(len(greeting)) #greeting stringinin uzunluğunu verir.
+#print(greeting[-1]) #son karakteri verir.
+#print(greeting[0:192]) 
+#print(greeting[0:192:2]) #0 ile 192 arasındaki karakterleri 2 atlayarak verir. 192 dahil değil.
+
+
+
+#ters slash \n yeni satır oluşturur. \t ise tab boşluğu oluşturur. \r ise imleci satır başına getirir. \\ ise tek ters slash yazdırır. \' tek tırnak, \" ise çift tırnak yazdırır.
