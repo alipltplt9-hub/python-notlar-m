c189d48dce6e61cfe3f0613706b362592ba2802d
alipltplt9-hub <alipltplt9@gmail.com>
2026-03-08 04:24:53 +0300

    Add files via upload

---
+++ string-methods.py
@@ -0,0 +1,28 @@
+from operator import index
+
+
+message = "Hello there.My name is ali polat"
+# #message = message.upper()#upper():Tüm harfleri büyük yapar
+# message = message.lower()#lower():Tüm harfleri küçük yapar
+# message = message.title()#title():Her kelimenin ilk harfini büyük yapar
+# message = message.capitalize()#capitalize():Sadece ilk harfi büyük yapar
+# message = message.strip()#strip():Başında ve sonunda boşlukları siler
+# message=message.split()#split():Cümleyi kelimelere ayırır
+# message=''.join(message)#join():Kelime listesini cümle yapar
+# message='---'.join(message)#join():Kelime listesini --- ile birleştirir
+# index = message.find('ali')#find():Verilen değerin kaçıncı indexte olduğunu verir
+# isFound = message.startswith('Hello')#startswith():Cümlenin verilen değerle başlayıp başlamadığını kontrol eder
+# isFound = message.endswith('polat')#endswith():Cümlenin verilen değerle bitip bitmediğini kontrol eder
+# message = message.replace('ali','veli')#replace():Cümledeki verilen değeri başka bir değerle değiştirir
+#  message = message.center(50,'*')#center():Cümleyi verilen karakterle ortalar
+ 
+print(message)
+
+
+print(index)
+
+
+
+
+#shift+alt+aşağı ok satırı alta kopyalar
+
